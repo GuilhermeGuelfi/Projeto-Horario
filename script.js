6ac3c8af34357fe9090d1e4b9f3eb890356c062b
GuilhermeGuelfi <98917451+GuilhermeGuelfi@users.noreply.github.com>
2022-05-12 10:59:05 -0400

    att

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ function informacoes() {
     var horario = data.getHours()
     horas.innerHTML = `Agora são ${horario} horas`
     if (horario >= 0 && horario <12) { //BOM DIA
-        img.innerHTML = "<img src='/imagens/amanhecer.jpg'>"
+        img.innerHTML = "<img src='/imagens/amanhecer.JPG'>"
         document.body.style.background = 'rgb(69, 69, 181)'
     } else if (horario >= 12 && horario <18) { //BOA TARDE
-        img.innerHTML = "<img src='/imagens/tarde.jpg'>"
+        img.innerHTML = "<img src='/imagens/tarde.JPG'>"
         document.body.style.background = 'rgb(197, 148, 49)'
     } else { //BOA NOITE
-        img.innerHTML = "<img src='/imagens/noite.jpg'>"
+        img.innerHTML = "<img src='/imagens/noite.JPG'>"
         document.body.style.background = 'rgb(61, 61, 61)'
     }
 }
